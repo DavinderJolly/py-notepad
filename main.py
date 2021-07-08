9e251f19df0ef44761e6fa5b7958c9bd58aea986
Abir Hasan <abir.spsc@gmail.com>
2021-07-08 18:09:06 +0600

    Add exit and status_bar handler

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from datetime import datetime
 
 from prompt_toolkit.application import Application
+from prompt_toolkit.application.current import get_app
 from prompt_toolkit.key_binding import KeyBindings
-from prompt_toolkit.layout.containers import HSplit, VSplit, Window
-from prompt_toolkit.widgets import MenuContainer, MenuItem, TextArea
+from prompt_toolkit.layout.containers import (
+    ConditionalContainer,
+    HSplit,
+    VSplit,
+    Window,
+)
+from prompt_toolkit.filters import Condition
+from prompt_toolkit.widgets import (
+    MenuContainer,
+    MenuItem,
+    TextArea,
+)
 from prompt_toolkit.layout.controls import FormattedTextControl
 from prompt_toolkit.layout.layout import Layout
 from prompt_toolkit.styles import Style
 from prompt_toolkit.lexers import PygmentsLexer
 from pygments.lexers.python import PythonLexer
+
+
+class ApplicationState:
+    "Application state"
+    show_status_bar = True
 
 
 # Editing area
@@ -19,15 +35,26 @@ text_field = TextArea(
 )
 
 
+# Status bar area
 def get_datetime():
     "Get opening datetime"
     return "Opened at " + datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
 
 
-statusbar_field = VSplit([Window(FormattedTextControl(get_datetime()))], height=1)
+status_bar_field = VSplit([Window(FormattedTextControl(get_datetime()))], height=1)
+
 
 # UI main body
-body = HSplit([text_field, statusbar_field])
+body = HSplit(
+    [
+        text_field,
+        ConditionalContainer(
+            content=status_bar_field,
+            filter=Condition(lambda: ApplicationState.show_status_bar),
+        ),
+    ]
+)
+
 
 # Keybindings
 bindings = KeyBindings()
@@ -46,6 +73,10 @@ def _focus(event):
 
 
 # Menu items
+def status_bar_handler():
+    ApplicationState.show_status_bar = not ApplicationState.show_status_bar
+
+
 root_container = MenuContainer(
     body=body,
     menu_items=[
@@ -54,10 +85,10 @@ root_container = MenuContainer(
             children=[
                 MenuItem("New"),
                 MenuItem("Save"),
-                MenuItem("Exit"),
+                MenuItem("Exit", handler=lambda: get_app().exit()),
             ],
         ),
-        MenuItem("View", children=[MenuItem("Status Bar")]),
+        MenuItem("View", children=[MenuItem("Status Bar", handler=status_bar_handler)]),
         MenuItem("Info", children=[MenuItem("About")]),
     ],
     key_bindings=bindings,
